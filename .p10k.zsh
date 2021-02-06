f2b05669b888e6d06af1acb51f5b7009d571064b
hskn-dev <hashiken.develop@gmail.com>
2021-02-06 11:30:42 +0900

    update

--- .p10k.zsh
+++ .p10k.zsh
@@ -179,7 +179,7 @@
 
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
-  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=232
+  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=239
   typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=250
   # Custom icon.
   # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
